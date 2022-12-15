599efe0e29db6c9ac34e4fc8883539eb0b5b2900
harkeswani <hk770@scarletmail.rutgers.edu>
2022-12-15 17:43:20 -0500

    Added variable disk size

--- testing/memgrinder.c
+++ testing/memgrinder.c
@@ -6,8 +6,8 @@
 
 int main(int argc, char *argv[]){
   char *disk = malloc(4096);
-  //createEmptyDisk("testdisk", disk);
-  wo_mount("testdisk", disk);
+  createEmptyDisk("testdisk", 4096, disk);
+  //wo_mount("testdisk", disk);
   printDisk(disk);
   wo_unmount(disk);
   free(disk);
--- writeonceFS.c
+++ writeonceFS.c
@@ -6,11 +6,13 @@
 
 struct diskinfo{
   char diskName[16];
-  unsigned int numFiles;
-  unsigned int numBlocksUsed;
+  unsigned int diskSize;
+  unsigned int usedSpace;
+  unsigned int usedBlocks;
+  unsigned int numBlocks;
   time_t createTime;
   time_t modifiedTime;
-  struct filenode *firstFile;
+  unsigned int firstFile;
   unsigned int blockTable[128]; // What blocks are used overall
 };
 
@@ -20,74 +22,136 @@ struct filenode{
   unsigned int permissions;
   time_t createTime;
   time_t modifiedTime;
-  struct filenode *nextFile;
+  unsigned int nextFile;
   unsigned int usedTable[128]; // What blocks this file occupies
 };
 
 struct fileDescriptor{ // Used only on runtime (not stored on disk)
   int fd;
   int currentPos;
-  void *fileAddr;
+  unsigned int locationBlock;
   struct fileDescriptor *next;
 };
 
 void *baseAddress; // Compute relative addresses on disk instead
 struct fileDescriptor *fdList;
+int currentFD = 0; // FD generation increments
 int diskMounted = 0; // 1 if disk is mounted
+struct diskinfo super;
 
 // Get and set bits from blocks
-int getBlock(int *table, int position){
+int getBlock(unsigned int *table, int position){
   return table[position/32]<<(position%32)>>31;
 }
-void setBlock(int *table, int position, int value){
+void setBlock(unsigned int *table, int position, int value){
   table[position/32] = (value==0) ? table[position/32]|(1<<(position%32)) : table[position/32] & ~(1<<(position%32));
 }
 
 void printDisk(void *memaddr){
-  struct diskinfo super;
-  memcpy(&super, memaddr, sizeof(struct diskinfo));
-  printf("Disk %s: %u Files, %u Blocks Used\n", super.diskName, super.numFiles, super.numBlocksUsed);
+  printf("Disk %s: %u/%u Bytes Used, %u/%u Blocks Used\n", super.diskName, super.usedSpace, super.diskSize, super.usedBlocks, super.numBlocks);
   printf("Created: %s",ctime(&super.createTime));
-  printf("Modified: %s\n",ctime(&super.modifiedTime));
+  printf("Modified: %s",ctime(&super.modifiedTime));
 }
 
-void createEmptyDisk(char *diskName, void *memaddr){
-  struct diskinfo super;
+void createEmptyDisk(char *diskName, unsigned int diskSize, void *memaddr){
   memcpy(super.diskName, diskName, strlen(diskName)+1);
-  super.numFiles = 0;
-  super.numBlocksUsed = 0;
-  super.firstFile = NULL;
+  super.firstFile = 0;
+  super.diskSize = diskSize;
+  super.usedSpace = 0;
+  super.usedBlocks = 1;
+  super.numBlocks = diskSize/1024;
   super.createTime = time(NULL);
   super.modifiedTime = time(NULL);
   setBlock(super.blockTable, 0, 1);
   memcpy(memaddr, &super, sizeof(struct diskinfo));
+  diskMounted = 1;
 }
 
 int wo_mount(char *filename, void *memaddr){
   FILE *fp = fopen(filename, "r");
-  fread(memaddr, 4096, 1, fp);
+  fread(&super, 1024, 1024, fp);
+  fclose(fp);
   baseAddress = memaddr;
+  fp = fopen(filename, "r");
+  fread(memaddr, super.diskSize, 1024, fp);
   fclose(fp);
   diskMounted = 1;
   return 0;
 }
 
 int wo_unmount(void *memaddr){
-  struct diskinfo dInfo;
-  memcpy(&dInfo, memaddr, sizeof(struct diskinfo));
-  dInfo.modifiedTime = time(NULL);
-  memcpy(memaddr, &dInfo, sizeof(struct diskinfo));
-  FILE *fp = fopen(dInfo.diskName, "w+");
-  fwrite(memaddr, 4096, 1, fp);
+  super.modifiedTime = time(NULL);
+  FILE *fp = fopen(super.diskName, "w+");
+  fwrite(memaddr, 1024, super.numBlocks, fp);
   fclose(fp);
   diskMounted = 0;
   return 0;
 }
 
-// RDONLY is 0, WRONLY is 1, RDWR is 2, CR is 100
-int wo_open(char *fileName, int flags, int mode){
-  if (mode==100){
-
+// Returns block number of file or 0 if not found
+unsigned int findFile(char *fileName){
+  unsigned int currentFile = super.firstFile;
+  if (currentFile==0){
+    return 0;
+  }
+  struct filenode current;
+  while (currentFile!=0 && currentFile<super.numBlocks){
+    memcpy(&current, baseAddress+(1024*currentFile),1024);
+    if (strcmp(current.name, fileName)==0){
+      return currentFile;
+    } else {
+      currentFile = current.nextFile;
+    }
   }
   return 0;
 }
+
+unsigned int getFirstFreeBlock(){
+  for (unsigned int i=1;i<super.numBlocks;i++){
+    if (getBlock(super.blockTable, 1)==0){
+      return i;
+    }
+  }
+  return 0;
+}
+
+int createFD(unsigned int fileBlock){
+  struct fileDescriptor *newFD = malloc(sizeof(struct fileDescriptor));
+  newFD->locationBlock = fileBlock;
+  newFD->currentPos = 0;
+  newFD->next = fdList;
+  newFD->fd = currentFD;
+  currentFD++;
+  fdList = newFD;
+  return newFD->fd;
+}
+
+// RDONLY is 0, WRONLY is 1, RDWR is 2, CR is 100
+int wo_open(char *fileName, int flags, int mode){
+  if (mode==100){
+    if (findFile(fileName)==0){
+      unsigned int freeBlock = getFirstFreeBlock();
+      if (freeBlock!=0){
+        struct filenode newFile;
+        memcpy(&newFile.name, fileName, 16);
+        newFile.size = 0;
+        newFile.permissions = 777;
+        newFile.createTime = time(NULL);
+        newFile.modifiedTime = time(NULL);
+        newFile.nextFile = 0;
+        memset(&newFile.usedTable, 0, 512);
+        memcpy(baseAddress+(1024*freeBlock), &newFile, 1024);
+        super.usedBlocks++;
+        return createFD(freeBlock);
+      } else {
+        return -1; // Not enough space, can't be created
+      }
+    } else {
+      return -1; // File already exists, can't be created
+    }
+  } else {
+    return -1;
+  }
+}
+
+
