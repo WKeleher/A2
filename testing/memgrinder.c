d90c1f5f761ffdc83dca2ccad585f5beb3e25db3
harkeswani <hk770@scarletmail.rutgers.edu>
2022-12-15 17:48:28 -0500

    Added variable disk size 2

--- testing/memgrinder.c
+++ testing/memgrinder.c
@@ -5,9 +5,12 @@
 #include "../writeonceFS.c"
 
 int main(int argc, char *argv[]){
-  char *disk = malloc(4096);
-  createEmptyDisk("testdisk", 4096, disk);
-  //wo_mount("testdisk", disk);
+  char *disk = malloc(32768);
+  //createEmptyDisk("testdisk", 32768, disk);
+  wo_mount("testdisk", disk);
+  printDisk(disk);
+  int fd = wo_open("file1.txt", 777, 100);
+  printf("fd: %d\n", fd);
   printDisk(disk);
   wo_unmount(disk);
   free(disk);
