d63092a9851d26e31f9c24bdb42a98c5ef3cd000
harkeswani <hk770@scarletmail.rutgers.edu>
2022-12-15 16:19:46 -0500

    More changes

--- writeonceFS.c
+++ writeonceFS.c
@@ -10,22 +10,45 @@ struct diskinfo{
   unsigned int numBlocksUsed;
   time_t createTime;
   time_t modifiedTime;
-  struct fileNode *firstFile;
+  struct filenode *firstFile;
+  unsigned int blockTable[128]; // What blocks are used overall
 };
 
 struct filenode{
   char name[16];
   unsigned int size;
+  unsigned int permissions;
   time_t createTime;
-  time_t modifyTime;
-  struct filenode *nextfile;
+  time_t modifiedTime;
+  struct filenode *nextFile;
+  unsigned int usedTable[128]; // What blocks this file occupies
 };
+
+struct fileDescriptor{ // Used only on runtime (not stored on disk)
+  int fd;
+  int currentPos;
+  void *fileAddr;
+  struct fileDescriptor *next;
+};
+
+void *baseAddress; // Compute relative addresses on disk instead
+struct fileDescriptor *fdList;
+int diskMounted = 0; // 1 if disk is mounted
+
+// Get and set bits from blocks
+int getBlock(int *table, int position){
+  return table[position/32]<<(position%32)>>31;
+}
+void setBlock(int *table, int position, int value){
+  table[position/32] = (value==0) ? table[position/32]|(1<<(position%32)) : table[position/32] & ~(1<<(position%32));
+}
 
 void printDisk(void *memaddr){
   struct diskinfo super;
   memcpy(&super, memaddr, sizeof(struct diskinfo));
   printf("Disk %s: %u Files, %u Blocks Used\n", super.diskName, super.numFiles, super.numBlocksUsed);
-  printf("Created: %s Modified: %s\n", ctime(&super.createTime), ctime(&super.modifiedTime));
+  printf("Created: %s",ctime(&super.createTime));
+  printf("Modified: %s\n",ctime(&super.modifiedTime));
 }
 
 void createEmptyDisk(char *diskName, void *memaddr){
@@ -35,13 +58,17 @@ void createEmptyDisk(char *diskName, void *memaddr){
   super.numBlocksUsed = 0;
   super.firstFile = NULL;
   super.createTime = time(NULL);
+  super.modifiedTime = time(NULL);
+  setBlock(super.blockTable, 0, 1);
   memcpy(memaddr, &super, sizeof(struct diskinfo));
 }
 
 int wo_mount(char *filename, void *memaddr){
   FILE *fp = fopen(filename, "r");
   fread(memaddr, 4096, 1, fp);
+  baseAddress = memaddr;
   fclose(fp);
+  diskMounted = 1;
   return 0;
 }
 
@@ -53,7 +80,14 @@ int wo_unmount(void *memaddr){
   FILE *fp = fopen(dInfo.diskName, "w+");
   fwrite(memaddr, 4096, 1, fp);
   fclose(fp);
+  diskMounted = 0;
   return 0;
 }
 
+// RDONLY is 0, WRONLY is 1, RDWR is 2, CR is 100
+int wo_open(char *fileName, int flags, int mode){
+  if (mode==100){
 
+  }
+  return 0;
+}
